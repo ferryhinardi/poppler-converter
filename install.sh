fb19cfa7a389e86977e73bf30d9f8348c856691c
Ferry Hinardi <ferry_hinardi@yahoo.co.id>
2020-11-12 13:10:28 +0700

    update script install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,10 @@ apt-get update
 apt-get upgrade
 apt-get install gcc
 apt-get install g++
+apt-get install cmake
 
-echo "============ C++ =================="
-ls /usr/include/c++/9.2.0
+echo "============ include =================="
+ls /usr/include
 
 # cd /app/
 # ls
@@ -18,22 +19,22 @@ ls /usr/include/c++/9.2.0
 
 # export CPLUS_INCLUDE_PATH=/usr/include/c++/9.2.0
 
-cd /app/
-wget https://cmake.org/files/v3.18/cmake-3.18.4.tar.gz
-tar -xzf cmake-3.18.4.tar.gz
-echo "============ ls app =================="
-ls
-cd cmake-3.18.4
-echo "============ ls cmake =================="
-ls
-sed -i '/"lib64"/s/64//' Modules/GNUInstallDirs.cmake &&
-./bootstrap --prefix=/usr        \
-            --system-libs        \
-            --mandir=/share/man  \
-            --no-system-jsoncpp  \
-            --no-system-librhash \
-            --docdir=/share/doc/cmake-3.18.4 &&
-make
+# cd /app/
+# wget https://cmake.org/files/v3.18/cmake-3.18.4.tar.gz
+# tar -xzf cmake-3.18.4.tar.gz
+# echo "============ ls app =================="
+# ls
+# cd cmake-3.18.4
+# echo "============ ls cmake =================="
+# ls
+# sed -i '/"lib64"/s/64//' Modules/GNUInstallDirs.cmake &&
+# ./bootstrap --prefix=/usr        \
+#             --system-libs        \
+#             --mandir=/share/man  \
+#             --no-system-jsoncpp  \
+#             --no-system-librhash \
+#             --docdir=/share/doc/cmake-3.18.4 &&
+# make
 
 cd /app/
 wget https://poppler.freedesktop.org/poppler-20.11.0.tar.xz
