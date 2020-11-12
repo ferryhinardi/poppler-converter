881d69f62497beb30436b30d182ebd302e441629
Ferry Hinardi <ferry_hinardi@yahoo.co.id>
2020-11-12 12:15:19 +0700

    update script install.sh

--- install.sh
+++ install.sh
@@ -3,38 +3,40 @@ apt install gcc
 
 cd /app/
 mkdir fontconfig-2.13.92
+ls
 cd fontconfig-2.13.92
 wget http://www.freedesktop.org/software/fontconfig/release/fontconfig-2.13.92.tar.gz
 tar -xzf fontconfig-2.13.92.tar.gz
+ls
 ./configure
 make
 
-cd /app/
-mkdir cmake-3.18.4
-cd cmake-3.18.4
-wget https://cmake.org/files/v3.18/cmake-3.18.4.tar.gz
-tar -xzf cmake-3.18.4.tar.gz
-sed -i '/"lib64"/s/64//' Modules/GNUInstallDirs.cmake &&
-./bootstrap --prefix=/usr        \
-            --system-libs        \
-            --mandir=/share/man  \
-            --no-system-jsoncpp  \
-            --no-system-librhash \
-            --docdir=/share/doc/cmake-3.18.4 &&
-make
+# cd /app/
+# mkdir cmake-3.18.4
+# cd cmake-3.18.4
+# wget https://cmake.org/files/v3.18/cmake-3.18.4.tar.gz
+# tar -xzf cmake-3.18.4.tar.gz
+# sed -i '/"lib64"/s/64//' Modules/GNUInstallDirs.cmake &&
+# ./bootstrap --prefix=/usr        \
+#             --system-libs        \
+#             --mandir=/share/man  \
+#             --no-system-jsoncpp  \
+#             --no-system-librhash \
+#             --docdir=/share/doc/cmake-3.18.4 &&
+# make
 
-cd /app/
-mkdir poppler-20.11.0
-cd poppler-20.11.0
-wget https://poppler.freedesktop.org/poppler-20.11.0.tar.xz
-tar -xzf poppler-20.11.0.tar.xz
-# FONTCONFIG_LIBS="-L/app/fontconfig-2.13.92/src/.libs/ -lfontconfig" FONTCONFIG_CFLAGS="-I/app/fontconfig-20.11.0/" ./configure
-mkdir build                         &&
-cd    build                         &&
+# cd /app/
+# mkdir poppler-20.11.0
+# cd poppler-20.11.0
+# wget https://poppler.freedesktop.org/poppler-20.11.0.tar.xz
+# tar -xzf poppler-20.11.0.tar.xz
+# # FONTCONFIG_LIBS="-L/app/fontconfig-2.13.92/src/.libs/ -lfontconfig" FONTCONFIG_CFLAGS="-I/app/fontconfig-20.11.0/" ./configure
+# mkdir build                         &&
+# cd    build                         &&
 
-cmake  -DCMAKE_BUILD_TYPE=Release   \
-       -DCMAKE_INSTALL_PREFIX=/usr  \
-       -DTESTDATADIR=$PWD/testfiles \
-       -DENABLE_UNSTABLE_API_ABI_HEADERS=ON     \
-       ..                           &&
-make
+# cmake  -DCMAKE_BUILD_TYPE=Release   \
+#        -DCMAKE_INSTALL_PREFIX=/usr  \
+#        -DTESTDATADIR=$PWD/testfiles \
+#        -DENABLE_UNSTABLE_API_ABI_HEADERS=ON     \
+#        ..                           &&
+# make
