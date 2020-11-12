c4ee7779084a77571452b7d0c15c9f8d736c2ef9
Ferry Hinardi <ferry_hinardi@yahoo.co.id>
2020-11-12 13:02:11 +0700

    update script install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 apt-get update
+apt-get upgrade
 apt-get install gcc
+apt-get install g++
+
+echo "============ C++ =================="
+ls /usr/include/c++/9.2.0
 
 # cd /app/
 # ls
@@ -11,13 +16,15 @@ apt-get install gcc
 # ./configure
 # make
 
+# export CPLUS_INCLUDE_PATH=/usr/include/c++/9.2.0
+
 cd /app/
 wget https://cmake.org/files/v3.18/cmake-3.18.4.tar.gz
 tar -xzf cmake-3.18.4.tar.gz
-echo "ls app"
+echo "============ ls app =================="
 ls
 cd cmake-3.18.4
-echo "ls cmake"
+echo "============ ls cmake =================="
 ls
 sed -i '/"lib64"/s/64//' Modules/GNUInstallDirs.cmake &&
 ./bootstrap --prefix=/usr        \
@@ -28,17 +35,17 @@ sed -i '/"lib64"/s/64//' Modules/GNUInstallDirs.cmake &&
             --docdir=/share/doc/cmake-3.18.4 &&
 make
 
-# cd /app/
-# wget https://poppler.freedesktop.org/poppler-20.11.0.tar.xz
-# tar -xf  poppler-20.11.0.tar.xz
-# echo "ls app"
-# ls
-# cd poppler-20.11.0
-# echo "ls poppler"
-# ls
-# cmake  -DCMAKE_BUILD_TYPE=Release   \
-#        -DCMAKE_INSTALL_PREFIX=/usr  \
-#        -DTESTDATADIR=$PWD/testfiles \
-#        -DENABLE_UNSTABLE_API_ABI_HEADERS=ON     \
-#        ..                           &&
-# make
+cd /app/
+wget https://poppler.freedesktop.org/poppler-20.11.0.tar.xz
+tar -xf  poppler-20.11.0.tar.xz
+echo "============ ls app =================="
+ls
+cd poppler-20.11.0
+echo "============ ls poppler =================="
+ls
+cmake  -DCMAKE_BUILD_TYPE=Release   \
+       -DCMAKE_INSTALL_PREFIX=/usr  \
+       -DTESTDATADIR=$PWD/testfiles \
+       -DENABLE_UNSTABLE_API_ABI_HEADERS=ON     \
+       ..                           &&
+make
