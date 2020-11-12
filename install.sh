ae793b46329d172ff2421a6a6c55521ae7b1c13c
Ferry Hinardi <ferry_hinardi@yahoo.co.id>
2020-11-12 12:50:33 +0700

    update script install.sh

--- install.sh
+++ install.sh
@@ -11,31 +11,34 @@ apt-get install gcc
 # ./configure
 # make
 
-# cd /app/
-# mkdir cmake-3.18.4
-# cd cmake-3.18.4
-# wget https://cmake.org/files/v3.18/cmake-3.18.4.tar.gz
-# tar -xzf cmake-3.18.4.tar.gz
-# sed -i '/"lib64"/s/64//' Modules/GNUInstallDirs.cmake &&
-# ./bootstrap --prefix=/usr        \
-#             --system-libs        \
-#             --mandir=/share/man  \
-#             --no-system-jsoncpp  \
-#             --no-system-librhash \
-#             --docdir=/share/doc/cmake-3.18.4 &&
-# make
-
 cd /app/
-wget https://poppler.freedesktop.org/poppler-20.11.0.tar.xz
-tar -xf  poppler-20.11.0.tar.xz
+wget https://cmake.org/files/v3.18/cmake-3.18.4.tar.gz
+tar -xzf cmake-3.18.4.tar.gz
 echo "ls app"
 ls
-cd poppler-20.11.0
-echo "ls poppler"
+cd cmake-3.18.4
+echo "ls cmake"
 ls
-cmake  -DCMAKE_BUILD_TYPE=Release   \
-       -DCMAKE_INSTALL_PREFIX=/usr  \
-       -DTESTDATADIR=$PWD/testfiles \
-       -DENABLE_UNSTABLE_API_ABI_HEADERS=ON     \
-       ..                           &&
+sed -i '/"lib64"/s/64//' Modules/GNUInstallDirs.cmake &&
+./bootstrap --prefix=/usr        \
+            --system-libs        \
+            --mandir=/share/man  \
+            --no-system-jsoncpp  \
+            --no-system-librhash \
+            --docdir=/share/doc/cmake-3.18.4 &&
 make
+
+# cd /app/
+# wget https://poppler.freedesktop.org/poppler-20.11.0.tar.xz
+# tar -xf  poppler-20.11.0.tar.xz
+# echo "ls app"
+# ls
+# cd poppler-20.11.0
+# echo "ls poppler"
+# ls
+# cmake  -DCMAKE_BUILD_TYPE=Release   \
+#        -DCMAKE_INSTALL_PREFIX=/usr  \
+#        -DTESTDATADIR=$PWD/testfiles \
+#        -DENABLE_UNSTABLE_API_ABI_HEADERS=ON     \
+#        ..                           &&
+# make
